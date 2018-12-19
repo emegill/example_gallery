9f651e856cf0160682d1f9f95660795c70dd0cb3
Ethan Megill <megill1352@gmail.com>
2018-12-18 19:55:23 -0500

    modal function

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 // python -m SimpleHTTPServer run in terminal
 
-const galleryContainer = document.querySelector('.gallery-container');
+const galleryContainer = document.querySelector('.gallery__container');
 const sortTitleBtn = document.querySelector('.sort__btn__title');
 const sortPriceBtn = document.querySelector('.sort__btn__price');
 const sortRatingBtn = document.querySelector('.sort__btn__rating');
-let tileData;
+let modal = document.querySelector(".modal");
+let modalContents = document.querySelector(".modal__content")
+let closeButton = document.querySelector(".modal__close__btn");
+let tileJsonData;
+
+function bindEventListeners() {
+    sortTitleBtn.addEventListener("click", runSortTitle);
+    sortPriceBtn.addEventListener("click", runSortPrice);
+    sortRatingBtn.addEventListener("click", runSortRating);
+    closeButton.addEventListener("click", toggleModal);
+}
 
 function createNode(element) {
     return document.createElement(element);
@@ -14,11 +24,11 @@ function append(parent, el) {
     return parent.appendChild(el);
 }
 
-function generateGalleryMarkup(tileData) {
+function generateGalleryMarkup(tileJsonData) {
     galleryContainer.innerHTML = '';
-    
-    for (let i = 0; i < tileData.length; i++) {
-        const tile = tileData[i];
+
+    for (let i = 0; i < tileJsonData.length; i++) {
+        const tile = tileJsonData[i];
 
         let listItem = createNode('li');
         let img = createNode('img');
@@ -32,6 +42,9 @@ function generateGalleryMarkup(tileData) {
         rating.innerHTML = 'Rating: ' + tile.rating;
         listItem.classList.add("w3-col", "w3-row-padding", "s12", "m6", "l4", "gallery__tile");
 
+        listItem.setAttribute('data-slug', tile.slug)
+        listItem.addEventListener("click", toggleModal);
+
         append(listItem, img);
         append(listItem, name);
         append(listItem, price);
@@ -41,21 +54,8 @@ function generateGalleryMarkup(tileData) {
     }
 }
 
-
-fetch('mock_data.json')
-    .then((resp) => resp.json())
-    .then(function(json) {
-        tileData = json.tiles;
-        generateGalleryMarkup(tileData);
-
-    })
-
-sortTitleBtn.addEventListener("click", runSortTitle);
-sortPriceBtn.addEventListener("click", runSortPrice);
-sortRatingBtn.addEventListener("click", runSortRating);
-
 function runSortTitle(e) {
-    tileData.sort(function(a, b) {
+    tileJsonData.sort(function(a, b) {
         var nameA = a.name.toLowerCase(),
             nameB = b.name.toLowerCase()
         if (nameA < nameB)
@@ -65,23 +65,64 @@ function runSortTitle(e) {
         return 0
     })
 
-    generateGalleryMarkup(tileData);
+    generateGalleryMarkup(tileJsonData);
 }
 
 function runSortPrice(e) {
-    tileData.sort(function(a, b) {
+    tileJsonData.sort(function(a, b) {
         return a.price - b.price
     })
 
-    generateGalleryMarkup(tileData);
-
+    generateGalleryMarkup(tileJsonData);
 }
 
 function runSortRating(e) {
-    tileData.sort(function(a, b) {
+    tileJsonData.sort(function(a, b) {
         return a.rating - b.rating
     })
 
-    generateGalleryMarkup(tileData);
-
+    generateGalleryMarkup(tileJsonData);
 }
+
+function generateGalleryModal(tileDetails) {
+    let img = createNode('img');
+    let description = createNode('p');
+
+    modalContents.innerHTML = '';
+    img.src = tileDetails.image;
+    img.classList.add("modal__img")
+    description.innerHTML = tileDetails.description;
+
+    append(modalContents, img);
+    append(modalContents, description);
+}
+
+function toggleModal(e) {
+    const modalIsOpen = modal.classList.contains('modal__show');
+
+    if(modalIsOpen) {
+        modalContents.innerHTML = '';
+    } else {
+        const currentTarget = e.currentTarget;
+        const slug = currentTarget.getAttribute('data-slug');
+        const tileDetails = tileJsonData.find(function(tile) {
+            return tile.slug === slug;
+        });
+
+        generateGalleryModal(tileDetails);
+    }
+
+    modal.classList.toggle("modal__show");
+}
+
+fetch('mock_data.json')
+    .then((resp) => resp.json())
+    .then(function(json) {
+        tileJsonData = json.tiles;
+        generateGalleryMarkup(tileJsonData);
+    });
+
+window.onload = function() {
+    bindEventListeners();  
+}
+
